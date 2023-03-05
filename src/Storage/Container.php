04b9f2e71e4ed05082e0f2f2837e0d113a6370cd
DemetriSam <samartsew@gmail.com>
2023-03-05 10:16:36 +0300

    storage

--- src/Storage/Container.php
+++ src/Storage/Container.php
@@ -4,4 +4,13 @@ namespace DemetriSam\FruitGarden\Storage;
 
 class Container
 {
+    public $name;
+    public $condition;
+    public $fruits;
+
+    public function __construct($name, $condition)
+    {
+        $this->name = $name;
+        $this->condition = $condition;
+    }
 }
--- src/Storage/Storage.php
+++ src/Storage/Storage.php
@@ -4,4 +4,16 @@ namespace DemetriSam\FruitGarden\Storage;
 
 class Storage
 {
+    public $containers;
+
+    public function __construct($config)
+    {
+        $containers = [];
+
+        foreach ($config as $container) {
+            $containers[] = new Container($container['name'], $container['condition']);
+        }
+
+        $this->containers = $containers;
+    }
 }
